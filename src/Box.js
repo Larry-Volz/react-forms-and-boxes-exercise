fe7d0b5f2500d1cf7e5936370c43d699a6b16cdb
Larry Volz <imaginologist@gmail.com>
2022-03-05 18:10:46 -0500

    Box component done

---
+++ src/Box.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Box({
+  id,
+  handleRemove,
+  width = 4,
+  height = 4,
+  backgroundColor = "green"
+}) {
+
+    return (
+        <div>
+            <div
+            style={{
+                height: `${height}em`,
+                width: `${width}em`,
+                backgroundColor
+            }}
+            />
+        </div>
+    )
+}
+
+export default Box;
